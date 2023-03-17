93f6138f9cfd778f7176250db6205c9010fd57d3
Eliezer <eliezerjs@live.com>
2023-03-17 09:52:57 -0300

    Inclusão do Projeto inicial.

---
+++ BlazorAppCurriculo/wwwroot/scripts.js
@@ -0,0 +1,62 @@
+﻿function playAudio() {
+    var audioElement = document.getElementById('audio-background');
+    audioElement.play();
+}
+
+function stopAudio() {
+    var audio = document.getElementById("audio-background");
+    audio.pause();
+    audio.currentTime = 0;
+}
+
+function addLight(x, y) {
+    const light = document.createElement("div");
+    light.className = "light";
+    light.style.left = x + "px";
+    light.style.top = y + "px";
+    document.querySelector(".lights").appendChild(light);
+
+    setTimeout(() => {
+        light.style.opacity = 1;
+    }, 10);
+
+    setTimeout(() => {
+        light.style.opacity = 0;
+    }, 1000);
+
+    setTimeout(() => {
+        light.remove();
+    }, 2000);
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function flashingLights() {
+    const lightsContainer = document.createElement("div");
+    lightsContainer.className = "lights";
+    document.body.appendChild(lightsContainer);
+
+    for (let i = 0; i < 50; i++) {
+        const x = randomInt(0, window.innerWidth);
+        const y = randomInt(0, window.innerHeight);
+        setTimeout(() => {
+            addLight(x, y);
+        }, i * 100);
+    }
+
+    setTimeout(() => {
+        lightsContainer.remove();
+    }, 5000);
+}
+
+function ToggleBackgroundAndWatermark() {
+    var body = document.getElementsByTagName("body")[0];
+
+    if (body.style.backgroundImage === "") {
+        body.style.backgroundImage = "url('../images/background.jpg')";
+    } else {
+        body.style.backgroundImage = "";
+    }
+}
